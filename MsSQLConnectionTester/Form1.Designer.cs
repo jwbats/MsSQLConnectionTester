7557f8bf0f76e31b799b45287ff22221b6b2e7d7
jwbats <jwbats@gmail.com>
2015-06-25 22:36:57 +0200

    Initial commit.

---
+++ MsSQLConnectionTester/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace MsSQLConnectionTester
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.lblDataSource = new System.Windows.Forms.Label();
+			this.tbDataSource = new System.Windows.Forms.TextBox();
+			this.tbInitialCatalog = new System.Windows.Forms.TextBox();
+			this.lblInitialCatalog = new System.Windows.Forms.Label();
+			this.tbUserId = new System.Windows.Forms.TextBox();
+			this.lblUserId = new System.Windows.Forms.Label();
+			this.tbPassword = new System.Windows.Forms.TextBox();
+			this.lblPassword = new System.Windows.Forms.Label();
+			this.btnTestConnection = new System.Windows.Forms.Button();
+			this.tbOutput = new System.Windows.Forms.TextBox();
+			this.SuspendLayout();
+			// 
+			// lblDataSource
+			// 
+			this.lblDataSource.AutoSize = true;
+			this.lblDataSource.Location = new System.Drawing.Point(12, 15);
+			this.lblDataSource.Name = "lblDataSource";
+			this.lblDataSource.Size = new System.Drawing.Size(70, 13);
+			this.lblDataSource.TabIndex = 0;
+			this.lblDataSource.Text = "Data Source:";
+			// 
+			// tbDataSource
+			// 
+			this.tbDataSource.Location = new System.Drawing.Point(88, 12);
+			this.tbDataSource.Name = "tbDataSource";
+			this.tbDataSource.Size = new System.Drawing.Size(261, 20);
+			this.tbDataSource.TabIndex = 1;
+			// 
+			// tbInitialCatalog
+			// 
+			this.tbInitialCatalog.Location = new System.Drawing.Point(88, 38);
+			this.tbInitialCatalog.Name = "tbInitialCatalog";
+			this.tbInitialCatalog.Size = new System.Drawing.Size(261, 20);
+			this.tbInitialCatalog.TabIndex = 3;
+			// 
+			// lblInitialCatalog
+			// 
+			this.lblInitialCatalog.AutoSize = true;
+			this.lblInitialCatalog.Location = new System.Drawing.Point(12, 41);
+			this.lblInitialCatalog.Name = "lblInitialCatalog";
+			this.lblInitialCatalog.Size = new System.Drawing.Size(73, 13);
+			this.lblInitialCatalog.TabIndex = 2;
+			this.lblInitialCatalog.Text = "Initial Catalog:";
+			// 
+			// tbUserId
+			// 
+			this.tbUserId.Location = new System.Drawing.Point(88, 64);
+			this.tbUserId.Name = "tbUserId";
+			this.tbUserId.Size = new System.Drawing.Size(261, 20);
+			this.tbUserId.TabIndex = 5;
+			// 
+			// lblUserId
+			// 
+			this.lblUserId.AutoSize = true;
+			this.lblUserId.Location = new System.Drawing.Point(12, 67);
+			this.lblUserId.Name = "lblUserId";
+			this.lblUserId.Size = new System.Drawing.Size(44, 13);
+			this.lblUserId.TabIndex = 4;
+			this.lblUserId.Text = "User Id:";
+			// 
+			// tbPassword
+			// 
+			this.tbPassword.Location = new System.Drawing.Point(88, 90);
+			this.tbPassword.Name = "tbPassword";
+			this.tbPassword.Size = new System.Drawing.Size(261, 20);
+			this.tbPassword.TabIndex = 7;
+			// 
+			// lblPassword
+			// 
+			this.lblPassword.AutoSize = true;
+			this.lblPassword.Location = new System.Drawing.Point(12, 93);
+			this.lblPassword.Name = "lblPassword";
+			this.lblPassword.Size = new System.Drawing.Size(56, 13);
+			this.lblPassword.TabIndex = 6;
+			this.lblPassword.Text = "Password:";
+			// 
+			// btnTestConnection
+			// 
+			this.btnTestConnection.Location = new System.Drawing.Point(15, 116);
+			this.btnTestConnection.Name = "btnTestConnection";
+			this.btnTestConnection.Size = new System.Drawing.Size(334, 23);
+			this.btnTestConnection.TabIndex = 8;
+			this.btnTestConnection.Text = "Test Connection";
+			this.btnTestConnection.UseVisualStyleBackColor = true;
+			this.btnTestConnection.Click += new System.EventHandler(this.btnTestConnection_Click);
+			// 
+			// tbOutput
+			// 
+			this.tbOutput.Location = new System.Drawing.Point(15, 145);
+			this.tbOutput.Multiline = true;
+			this.tbOutput.Name = "tbOutput";
+			this.tbOutput.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+			this.tbOutput.Size = new System.Drawing.Size(334, 346);
+			this.tbOutput.TabIndex = 9;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(368, 503);
+			this.Controls.Add(this.tbOutput);
+			this.Controls.Add(this.btnTestConnection);
+			this.Controls.Add(this.tbPassword);
+			this.Controls.Add(this.lblPassword);
+			this.Controls.Add(this.tbUserId);
+			this.Controls.Add(this.lblUserId);
+			this.Controls.Add(this.tbInitialCatalog);
+			this.Controls.Add(this.lblInitialCatalog);
+			this.Controls.Add(this.tbDataSource);
+			this.Controls.Add(this.lblDataSource);
+			this.Name = "Form1";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "MsSQL Connection Tester";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label lblDataSource;
+		private System.Windows.Forms.TextBox tbDataSource;
+		private System.Windows.Forms.TextBox tbInitialCatalog;
+		private System.Windows.Forms.Label lblInitialCatalog;
+		private System.Windows.Forms.TextBox tbUserId;
+		private System.Windows.Forms.Label lblUserId;
+		private System.Windows.Forms.TextBox tbPassword;
+		private System.Windows.Forms.Label lblPassword;
+		private System.Windows.Forms.Button btnTestConnection;
+		private System.Windows.Forms.TextBox tbOutput;
+	}
+}
+
---
+++ MsSQLConnectionTester/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MsSQLConnectionTester
+{
+	public partial class Form1 : Form
+	{
+
+		#region ================================================== Private Members ==================================================
+
+		private string Connection
+		{
+			get
+			{
+				return ConfigurationManager.ConnectionStrings["connection"].ConnectionString;
+			}
+		}
+
+		#endregion ================================================== Private Members ==================================================
+
+
+
+
+		#region ================================================== Constructor ==================================================
+
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+
+
+		#endregion ================================================== Constructor ==================================================
+
+
+
+
+		#region ================================================== Private Methods ==================================================
+
+		private void tbSelectAllOnEnter(Object sender, EventArgs e)
+		{
+			(sender as TextBox).SelectAll();
+		}
+
+		#endregion ================================================== Private Methods ==================================================
+
+
+
+
+		#region ================================================== Public Methods ==================================================
+
+		private void _IsConnectionValid(string host, string database, string user, string password)
+		{
+			string connection = String.Format(Connection, host, database, user, password);
+
+			using (SqlConnection sqlConnection = new SqlConnection(connection))
+			{
+				sqlConnection.Open();
+			}			
+		}
+
+
+	
+		
+		private string IsConnectionValid(string dataSource, string database, string userId, string password)
+		{
+			try
+			{
+				_IsConnectionValid(dataSource, database, userId, password);
+				return "Connection is valid.";
+			}
+			catch (Exception exception)
+			{
+				return exception.ToString();
+			}
+		}
+
+
+	
+		
+		private void btnTestConnection_Click(object sender, EventArgs e)
+		{
+			string dataSource = tbDataSource.Text;
+			string database   = tbInitialCatalog.Text;
+			string userId     = tbUserId.Text;
+			string password   = tbPassword.Text;
+
+			tbOutput.Text = IsConnectionValid(dataSource, database, userId, password);
+		}
+
+		#endregion ================================================== Public Methods ==================================================
+
+	}
+}
